77be6146ab5273b150dcad2d2b552c2eae93cb8e
mikefelix <michael.felix@gmail.com>
2013-01-20 14:20:54 -0700

    Renook

---
+++ src/com/example/ClockActivity.java
@@ -0,0 +1,122 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.PowerManager;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClockActivity extends Activity {
+    private MyTimer timer;
+    private DateFormat timeFormat = new SimpleDateFormat("h:mm");
+    private DateFormat hourFormat = new SimpleDateFormat("h");
+    private DateFormat paddedHourFormat = new SimpleDateFormat(" h");
+    private DateFormat minuteFormat = new SimpleDateFormat(":mm");
+    private DateFormat dateFormat = new SimpleDateFormat("MMM d");
+    private PowerManager.WakeLock wakeLock;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "renook");
+        wakeLock.acquire();
+
+        setViews();
+
+        timer = new MyTimer();
+        timer.start();
+    }
+
+    private String getTemp(){
+        URL url;
+        BufferedReader reader = null;
+        String line;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            url = new URL("http://weather.yahooapis.com/forecastrss?w=12794144");
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        catch (MalformedURLException mue) {
+             mue.printStackTrace();
+        }
+        catch (IOException ioe) {
+             ioe.printStackTrace();
+        }
+        finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            }
+            catch (IOException ioe) {
+                // nothing to see here
+            }
+        }
+
+        Pattern pattern = Pattern.compile(".*temp=\"([^\"]+)\".*");
+        Matcher matcher = pattern.matcher(sb.toString());
+        if (!matcher.matches())
+            return "--";
+
+        return matcher.group(1) + "°";
+    }
+
+    private void setViews(){
+        TextView hour = (TextView) findViewById(R.id.hour);
+        TextView minutes = (TextView) findViewById(R.id.minutes);
+        TextView date = (TextView) findViewById(R.id.date);
+        Date dateTime = new Date();
+
+        String hourText = hourFormat.format(dateTime);
+        if (hourText.length() == 1)
+            hourText = " " + hourText;
+
+        hour.setText(hourText);
+        minutes.setText(minuteFormat.format(dateTime));
+//        date.setText(dateFormat.format(dateTime) + " / " + getTemp());
+        date.setText(dateFormat.format(dateTime) + "  ·  " + getTemp());
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        wakeLock.release();
+    }
+
+    private class MyTimer extends CountDownTimer {
+        static final long period = 1000 * 60;
+
+        public MyTimer() {
+            super(period, period);
+        }
+
+        @Override public void onTick(long l) {
+            setViews();
+        }
+
+        @Override public void onFinish() {
+            timer = new MyTimer();
+            timer.start();
+        }
+    }
+}
---
+++ src/com/example/QuoteActivity.java
@@ -0,0 +1,112 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.PowerManager;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class QuoteActivity extends Activity {
+    private final Quote[] quotes = new Quote[] {
+            new Quote("The first principle is that you must not fool yourself and you are the easiest person to fool.", "Richard P. Feynman", 3f, 2f, "justify"),
+            new Quote("It doesn't matter how beautiful your theory is, it doesn't matter how smart you are. If it doesn't agree with experiment, it's wrong.", "Richard P. Feynman", 3f, 2f, "justify"),
+            new Quote("What can be asserted without proof can be dismissed without proof.", "Christopher Hitchens", 3.5f, 2f, "justify"),
+    };
+
+    private MyTimer timer;
+    private int quoteNum = 0;
+    PowerManager.WakeLock wakeLock;
+
+    private static class Quote {
+        private Quote(String text, String byline, float quoteSize, float bylineSize, String justify) {
+            this.text = text;
+            this.byline = byline;
+            this.quoteSize = quoteSize;
+            this.bylineSize = bylineSize;
+            this.justify = justify;
+        }
+
+        String text, byline, justify;
+        float quoteSize, bylineSize;
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "renook");
+        wakeLock.acquire();
+
+        setupWebView();
+        showWebQuote();
+        timer = new MyTimer();
+        timer.start();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        wakeLock.release();
+    }
+
+    private void setupMainView() {
+        setContentView(R.layout.main);
+        findViewById(R.id.screen).setBackgroundColor(android.R.color.white);
+
+        TextView quote = (TextView) findViewById(R.id.quote);
+        TextView byline = (TextView) findViewById(R.id.byline);
+
+        quote.setBackgroundColor(android.R.color.white);
+        byline.setBackgroundColor(android.R.color.white);
+
+        quote.setText(quotes[quoteNum].text);
+        byline.setText(quotes[quoteNum].byline);
+    }
+
+    private void setupWebView() {
+        setContentView(R.layout.web);
+
+        WebView view = new WebView(this);
+        view.setId(R.id.quote);
+        view.setVerticalScrollBarEnabled(false);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.screen);
+        layout.addView(view);
+        layout.setBackgroundColor(android.R.color.white);
+    }
+
+    private void showWebQuote() {
+        Quote quote = quotes[quoteNum];
+        WebView view = (WebView) findViewById(R.id.quote);
+        view.loadData(getString(R.string.html).replaceAll("@quote@", quote.text)
+                              .replaceAll("@byline@", quote.byline)
+                              .replaceAll("@just@", quote.justify)
+                              .replaceAll("@qsize@", "" + quote.quoteSize)
+                              .replaceAll("@bsize@", "" + quote.bylineSize),
+                      "text/html", "utf-8");
+
+        quoteNum = (quoteNum + 1) % quotes.length;
+    }
+
+    private class MyTimer extends CountDownTimer {
+        static final long period = 1000 * 60 * 30;
+
+        public MyTimer() {
+            super(period, period);
+        }
+
+        @Override public void onTick(long l) {
+            showWebQuote();
+        }
+
+        @Override public void onFinish() {
+            timer = new MyTimer();
+            timer.start();
+        }
+    }
+}
